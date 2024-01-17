e694499a3383e4e0c7e9f1a540e5c011b97ed810
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-01-17 20:31:05 +0000

    showing the clusters of students across countries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,38 @@
 import './App.css';
-//import AppNavBar from './components/AppNavBar.js'
-//import Alumni from './components/Alumni.js'
-import React, {useState, useMemo} from 'react';
-import CITIES from './cities.json';
+import React, { useRef } from 'react';
 //import ControlPanel from './components/control-panel';
-import Pin from './components/pin';
-import Map, {
-  Marker,
-  Popup,
-  NavigationControl,
-  FullscreenControl,
-  ScaleControl,
-  GeolocateControl
-} from 'react-map-gl';
+import {Map, Source, Layer} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { clusterLayer, clusterCountLayer, unclusterPointLayer } from './components/layers';
+import alumniPerCountry from './countriesGeoJSON.json'
 
 const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 function App() {
-  const [popupInfo, setPopupInfo] = useState(null);
+  const mapRef = useRef(null);
 
-  const pins = useMemo(
-    () =>
-      CITIES.map((city, index) => (
-        <Marker
-          key={`marker-${index}`}
-          longitude={city.longitude}
-          latitude={city.latitude}
-          anchor="bottom"
-          onClick={e => {
-            // If we let the click event propagates to the map, it will immediately close the popup
-            // with `closeOnClick: true`
-            e.originalEvent.stopPropagation();
-            setPopupInfo(city);
-          }}
-        >
-          <Pin />
-        </Marker>
-      )),
-    []
-  );
+  const onClick = event => {
+    if (event.features && event.features.length > 0) {
+      const feature = event.features[0];
+      console.log("feature: ", feature)
+      const clusterId = feature.properties.cluster_id;
+
+      const mapboxSource = mapRef.current.getSource('alumniPerCountry');
+      
+      mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+        if(err){
+          return;
+        }
+
+        mapRef.current.easeTo({
+          center: feature.geometry.coordinates,
+          zoom,
+          duration: 500
+        });
+      });
+    }
+  }
+  
 
   return (
     <>
@@ -52,44 +45,34 @@ function App() {
       <div style={{height: '100vh'}}>
         <Map
           initialViewState={{
-            latitude: 40,
-            longitude: -100,
-            zoom: 3.5,
-            bearing: 0,
-            pitch: 0
+            latitude: 40.67,
+            longitude: -103.59,
+            zoom: 3
           }}
           mapStyle="mapbox://styles/mapbox/dark-v9"
           mapboxAccessToken={TOKEN}
+          interactiveLayerIds={[clusterLayer.id]}
+          onClick={onClick}
+          ref={mapRef}
         >
-          <GeolocateControl position="top-left" />
-          <FullscreenControl position="top-left" />
-          <NavigationControl position="top-left" />
-          <ScaleControl />
-
-          {pins}
-
-          {popupInfo && (
-            <Popup
-              anchor="top"
-              longitude={Number(popupInfo.longitude)}
-              latitude={Number(popupInfo.latitude)}
-              onClose={() => setPopupInfo(null)}
-            >
-              <div>
-                {popupInfo.city}, {popupInfo.state} |{' '}
-                <a
-                  target="_new"
-                  href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
-                >
-                  Wikipedia
-                </a>
-              </div>
-              <img width="100%" src={popupInfo.image} alt=''/>
-            </Popup>
-          )}
+          <Source
+            id="alumniPerCountry"
+            type="geojson"
+            data={alumniPerCountry}
+            cluster={true}
+            clusterMaxZoom={14}
+            clusterRadius={50}
+            clusterProperties={{
+              students: ['+', ['get', 'students']],
+            }}
+          >
+            <Layer {...clusterLayer}/>
+            <Layer {...clusterCountLayer}/>
+            <Layer {...unclusterPointLayer}/>
+          </Source>
         </Map>
       </div>
-
+      
       {/* <ControlPanel />*/}
     </>
   );
