f1495123b3d7c81b3a1b786032d09ad7896c4e22
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-01-17 20:31:05 +0000

    showing the clusters of students across countries

--- frontend/src/components/control-panel.js
+++ frontend/src/components/control-panel.js
@@ -1,29 +1,20 @@
 import * as React from 'react';
 
 function ControlPanel() {
-  return (
-    <div className="control-panel">
-      <h3>Marker, Popup, NavigationControl and FullscreenControl </h3>
-      <p>
-        Map showing top 20 most populated cities of the United States. Click on a marker to learn
-        more.
-      </p>
-      <p>
-        Data source:{' '}
-        <a href="https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population">
-          Wikipedia
-        </a>
-      </p>
-      <div className="source-link">
-        <a
-          href="https://github.com/visgl/react-map-gl/tree/7.1-release/examples/controls"
-          target="_new"
-        >
-          View Code ↗
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div className="control-panel">
+            <h3>Create and Style Clusters</h3>
+            <p>Use Mapbox GL JS' built-in functions to visualize points as clusters.</p>
+            <div className="source-link">
+                <a
+                href="https://github.com/visgl/react-map-gl/tree/7.1-release/examples/clusters"
+                target="_new"
+                >
+                View Code ↗
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default React.memo(ControlPanel);
