bf524f69ba36eb654cd97f3bfc868f43be339d01
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-01-17 14:24:20 +0000

    shows the map with pins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,97 @@
 import './App.css';
-import AppNavBar from './components/AppNavBar.js'
-import Alumni from './components/Alumni.js'
+//import AppNavBar from './components/AppNavBar.js'
+//import Alumni from './components/Alumni.js'
+import React, {useState, useMemo} from 'react';
+import CITIES from './cities.json';
+//import ControlPanel from './components/control-panel';
+import Pin from './components/pin';
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl
+} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
+const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 function App() {
+  const [popupInfo, setPopupInfo] = useState(null);
+
+  const pins = useMemo(
+    () =>
+      CITIES.map((city, index) => (
+        <Marker
+          key={`marker-${index}`}
+          longitude={city.longitude}
+          latitude={city.latitude}
+          anchor="bottom"
+          onClick={e => {
+            // If we let the click event propagates to the map, it will immediately close the popup
+            // with `closeOnClick: true`
+            e.originalEvent.stopPropagation();
+            setPopupInfo(city);
+          }}
+        >
+          <Pin />
+        </Marker>
+      )),
+    []
+  );
+
   return (
-    <div className="App">
-      <AppNavBar/>
-      <Alumni/>
-    </div>
+    <>
+      {/*
+      <div className="App">
+        <AppNavBar/>
+        <Alumni/>
+      </div>
+      */}
+      <div style={{height: '100vh'}}>
+        <Map
+          initialViewState={{
+            latitude: 40,
+            longitude: -100,
+            zoom: 3.5,
+            bearing: 0,
+            pitch: 0
+          }}
+          mapStyle="mapbox://styles/mapbox/dark-v9"
+          mapboxAccessToken={TOKEN}
+        >
+          <GeolocateControl position="top-left" />
+          <FullscreenControl position="top-left" />
+          <NavigationControl position="top-left" />
+          <ScaleControl />
+
+          {pins}
+
+          {popupInfo && (
+            <Popup
+              anchor="top"
+              longitude={Number(popupInfo.longitude)}
+              latitude={Number(popupInfo.latitude)}
+              onClose={() => setPopupInfo(null)}
+            >
+              <div>
+                {popupInfo.city}, {popupInfo.state} |{' '}
+                <a
+                  target="_new"
+                  href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
+                >
+                  Wikipedia
+                </a>
+              </div>
+              <img width="100%" src={popupInfo.image} alt=''/>
+            </Popup>
+          )}
+        </Map>
+      </div>
+
+      {/* <ControlPanel />*/}
+    </>
   );
 }
 
