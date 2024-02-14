0cbf0c66ba2e289b36b72aa86f8cc6e148e58b23
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-02-14 11:42:28 +0000

    refactor read from api and write to excel

--- backend/src/main/java/com/feupAlumni/alumniFEUP/service/ApiDataAnalysisServiceImpl.java
+++ backend/src/main/java/com/feupAlumni/alumniFEUP/service/ApiDataAnalysisServiceImpl.java
@@ -8,6 +8,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.feupAlumni.alumniFEUP.handlers.FilesHandler;
+import com.feupAlumni.alumniFEUP.handlers.ManageApiData;
 import com.feupAlumni.alumniFEUP.model.Alumni;
 import com.feupAlumni.alumniFEUP.repository.AlumniRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,62 +28,23 @@ public class ApiDataAnalysisServiceImpl implements ApiDataAnalysisService {
     @Autowired
     private AlumniRepository alumniRepository;
 
-    boolean test = false;
-
     @Override
     public byte[] alumniTableToExcel(MultipartFile file) {
 
         // Load the Excel file
+        Workbook workbook = null;
         try (InputStream inputStream = file.getInputStream()) {
             // Read and iterate over the excel file
-            Workbook workbook = new XSSFWorkbook(inputStream);
+            workbook = new XSSFWorkbook(inputStream);
 
             Sheet sheet = workbook.getSheetAt(0);   // 1st sheet
             // 1st on the sublist is the row where which title should be 
-            String[][] fields = {
-                {"0", "Not_Subtitles","Linkedin Link", "public_identifier", "profile_pic_url", "background_cover_image_url", "first_name", "last_name", "full_name", "follower_count", "occupation", "headline", "summary", "country", "country_full_name", "city", "state"},
-                {"0", "Has_Subtitles", "experiences"},
-                {"1", "Expirience_Subtitle", "time", "company", "company_linkedin_profile_url", "title", "description", "location", "logo_url"},
-                {"0", "Not_Subtitles", "education"},
-                {"1", "Education_Subtitle", "time", "field_of_study", "degree_name", "school", "school_linkedin_profile_url", "description", "logo_url", "grade", "activities_and_societies"},
-                {"0", "Not_Subtitles", "languages"},
-                {"0", "Has_Subtitles", "accomplishment_organisations"},
-                {"1", "AccomplishmentOrganisations_subtitle", "time", "org_name", "title", "description"},
-                {"0", "Has_Subtitles", "accomplishment_publications"},
-                {"1", "AccomplishmentPublications_subtitle", "name", "publisher", "published_on", "description", "url"},
-                {"0", "Has_Subtitles", "accomplishment_honors_awards"},
-                {"1", "AccomplishmentHonorsAwards_subtitle", "title", "issuer", "issued_on", "description"},
-                {"0", "Has_Subtitles", "accomplishment_patents"},
-                {"1", "AccomplishmentPatents_subtitle", "title", "issuer", "issued_on", "description", "application_number", "patent_number", "url"},
-                {"0", "Has_Subtitles", "accomplishment_courses"},
-                {"1", "AccomplishmentCourses_subtitle", "name", "number"},
-                {"0", "Has_Subtitles", "accomplishment_projects"},
-                {"1", "AccomplishmentProjects_subtitle", "time", "title", "description", "url"},
-                {"0", "Has_Subtitles", "accomplishment_test_scores"},
-                {"1", "AccomplishmentTestScores_subtitle", "name", "score", "date_on", "description"},
-                {"0", "Has_Subtitles", "volunteer_work"},
-                {"1", "VolunteerWork_subtitle", "time", "title", "cause", "company", "company_linkedin_profile_url", "description", "logo_url"},
-                {"0", "Has_Subtitles", "certifications"},
-                {"1", "Certifications_Subtitle", "time", "name", "license_number", "display_source", "authority", "url"},
-                {"0", "Not_Subtitles", "connections"},
-                {"0", "Has_Subtitles", "people_also_viewed"}, 
-                {"1", "PeopleAlsoViewed_subtitle", "link", "name", "summary", "location"},
-                {"0", "Not_Subtitles", "recommendations"},
-                {"0", "Has_Subtitles", "activities"},
-                {"1", "Activities_subtitle", "title", "link", "activity_status"},
-                {"0", "Has_Subtitles", "similarly_named_profiles"},
-                {"1", "SimilarlyNamedProfiles_subtitle", "name", "link", "summary", "location"},
-                {"0", "Has_Subtitles", "articles"},
-                {"1", "Articles_subtitle", "title", "link", "published_date", "author", "image_url"},
-                {"0", "Has_Subtitles", "groups"},
-                {"1", "Groups_Subtitle", "profile_pic_url", "name", "url"},
-                {"0", "Not_Subtitles", "skills", "inferred_salary", "gender", "birth_date", "industry", "extra", "interests", "personal_emails", "personal_numbers"}
-            };
+            String[][] fields = ManageApiData.getFields();
             
             // Write Excel Titles
             createHeaders(sheet, fields);
 
-            // Iterate over each row of the excel
+            // Iterate over each row of the excel and writes the content of the tiles written before
             Iterator<Alumni> alumniIterator = alumniRepository.findAll().iterator();
             int rowIndex=2;
             while (alumniIterator.hasNext()) {
@@ -102,9 +64,19 @@ public class ApiDataAnalysisServiceImpl implements ApiDataAnalysisService {
         } catch (IOException e) {
             e.printStackTrace();
             return null;
+        } finally {
+            // Close the workbook in the finally block to ensure it's always closed
+            if (workbook != null) {
+                try {
+                    workbook.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
+    // Create headers in the Excel file based on a given set of titles
     private void createHeaders(Sheet sheet, String[][] fields) {
         var columnCounter = 0;
         for (int i=0; i<fields.length; i++) {
@@ -113,7 +85,7 @@ public class ApiDataAnalysisServiceImpl implements ApiDataAnalysisService {
         }
     }
 
-    // Create a row
+    // Create a row header
     private void createHeaderRow (Sheet sheet, int rowIndex, int startColumn, String[] titles) {
         Row headerRow = sheet.getRow(rowIndex); // Create a new row
         if (headerRow == null) {
@@ -126,9 +98,42 @@ public class ApiDataAnalysisServiceImpl implements ApiDataAnalysisService {
         }
     }
 
-    // Writes the main titles. Returns the last written row. 
+    // Writes the alumni data to the row
+    private int writeAlumniDataToRow (Alumni alumni, int rowIndex, String linkedinInfo, Sheet sheet, String[][] fields) {
+        try {
+            int columnIndex = 1;
+            List<Integer> lastRowsWritten = new ArrayList<>();     
+            for (int i=0; i<fields.length; i++) {
+                Integer lastWrittenRow = 0;
+                if (fields[i][1] != "Has_Subtitles") { 
+                    if (fields[i][0] == "0") {
+                        lastWrittenRow = writeFieldMainTitleContent(alumni, sheet, rowIndex, columnIndex, fields[i], linkedinInfo);
+                    } else {
+                        List<ObjectNode> valuesSubtitles = ManageApiData.getValuesSubtitles(fields[i][1], linkedinInfo);
+                        lastWrittenRow = writeFieldSubtitleContent(sheet, valuesSubtitles, fields[i], columnIndex, rowIndex);
+                    }
+                    lastRowsWritten.add(lastWrittenRow);
+                    if (columnIndex == 1) {
+                        columnIndex = (columnIndex + (fields[i].length-2)) - 1; // fields[i].length-2 because the first 2 positions are not title. Decrements 1 in the first iteration so it starts the count with value 0
+                    } else {
+                        columnIndex = (columnIndex + (fields[i].length-2)); // fields[i].length-2 because the first 2 positions are not title
+                    }
+                } else {
+                    columnIndex++;
+                }
+            }
+            // Returns the last written row
+            int maxValue = Collections.max(lastRowsWritten);  
+            return maxValue;
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+            return 0;
+        }
+    }
+
+    // Writes the main titles values. Returns the last written row. 
     private int writeFieldMainTitleContent (Alumni alumni, Sheet sheet, int rowIndex, int columnIndex, String[] titles, String linkedinInfo) {
-       int lastWrittenRow = rowIndex;       
+        int lastWrittenRow = rowIndex;       
         // Create a new row
         Row row = sheet.getRow(rowIndex); 
         if (row == null) {
@@ -150,7 +155,7 @@ public class ApiDataAnalysisServiceImpl implements ApiDataAnalysisService {
         return lastWrittenRow;
     }
 
-    // Writes the sub titles. Returns the last written row
+    // Writes the sub titles values. Returns the last written row
     private int writeFieldSubtitleContent (Sheet sheet, List<ObjectNode> valuesSubtitles, String[] titles, int columnIndex, int rowIndex) {
         int lastWrittenRow = rowIndex;
         // Write values from experiencesList under experienceTitles
@@ -163,7 +168,10 @@ public class ApiDataAnalysisServiceImpl implements ApiDataAnalysisService {
 
             for (int i=2; i<titles.length; i++) {
                 String fieldName = titles[i];
-                String fieldValue = valueSubtitles.get(fieldName).asText();
+                String fieldValue = "";
+                if (valueSubtitles != null) {
+                    fieldValue = valueSubtitles.get(fieldName).asText();
+                }
                 Cell cell = rowExperience.createCell(columnIndex + (i-2)); // i-2 because I want to start on the write length of the fields before
                 cell.setCellValue(fieldValue);
             }
@@ -172,88 +180,4 @@ public class ApiDataAnalysisServiceImpl implements ApiDataAnalysisService {
         return lastWrittenRow;
     }
 
-    // Writes the alumni data to the row
-    private int writeAlumniDataToRow (Alumni alumni, int rowIndex, String linkedinInfo, Sheet sheet, String[][] fields) {
-        try {
-            int columnIndex = 1;
-            List<Integer> lastRowsWritten = new ArrayList<>();     
-            for (int i=0; i<fields.length; i++) {
-                Integer lastWrittenRow = 0;
-                if (fields[i][1] != "Has_Subtitles") { 
-                    if (fields[i][0] == "0") {
-                        lastWrittenRow = writeFieldMainTitleContent(alumni, sheet, rowIndex, columnIndex, fields[i], linkedinInfo);
-                    } else {
-                        List<ObjectNode> valuesSubtitles = new ArrayList<>();
-                        switch (fields[i][1]) {
-                            case "Expirience_Subtitle":
-                                valuesSubtitles = FilesHandler.getExperienceDetails(linkedinInfo);
-                                break;
-                            case "Education_Subtitle":
-                                valuesSubtitles = FilesHandler.getEducationDetails(linkedinInfo);
-                                break;
-                            case "AccomplishmentOrganisations_subtitle":
-                                valuesSubtitles = FilesHandler.getAccompOrganisationsDetails(linkedinInfo);
-                                break;
-                            case "AccomplishmentPublications_subtitle":
-                                valuesSubtitles = FilesHandler.getAccompPublicationsDetails(linkedinInfo);
-                                break;
-                            case "AccomplishmentHonorsAwards_subtitle":
-                                valuesSubtitles = FilesHandler.getAccompHonorsAwardsDetails(linkedinInfo);
-                                break;
-                            case "AccomplishmentPatents_subtitle":
-                                valuesSubtitles = FilesHandler.getAccompPatentsDetails(linkedinInfo);
-                                break;
-                            case "AccomplishmentCourses_subtitle":
-                                valuesSubtitles = FilesHandler.getAccompCoursesDetails(linkedinInfo);
-                                break;
-                            case "AccomplishmentProjects_subtitle":
-                                valuesSubtitles = FilesHandler.getAccompProjectsDetails(linkedinInfo);
-                                break;
-                            case "AccomplishmentTestScores_subtitle":
-                                valuesSubtitles = FilesHandler.getAccompTestScoresDetails(linkedinInfo);
-                                break;
-                            case "VolunteerWork_subtitle":
-                                valuesSubtitles = FilesHandler.getVolunterWorksDetails(linkedinInfo);
-                                break;
-                            case "Certifications_Subtitle":
-                                valuesSubtitles = FilesHandler.getCertificationsDetails(linkedinInfo);
-                                break;
-                            case "PeopleAlsoViewed_subtitle":
-                                valuesSubtitles = FilesHandler.getPeopleAlsoViewedDetails(linkedinInfo);
-                                break;
-                            case "Activities_subtitle":
-                                valuesSubtitles = FilesHandler.getActivitiesDetails(linkedinInfo);
-                                break;
-                            case "SimilarlyNamedProfiles_subtitle":
-                                valuesSubtitles = FilesHandler.getSimilarlyNamedProfilesDetails(linkedinInfo);
-                                break;
-                            case "Articles_subtitle":
-                                valuesSubtitles = FilesHandler.getArticlesDetails(linkedinInfo);
-                                break;
-                            case "Groups_Subtitle":
-                                valuesSubtitles = FilesHandler.getGroupsDetails(linkedinInfo);
-                                break;
-                            default:
-                                System.out.println("TITLE NOT VALID");
-                        }
-                        lastWrittenRow = writeFieldSubtitleContent(sheet, valuesSubtitles, fields[i], columnIndex, rowIndex);
-                    }
-                    lastRowsWritten.add(lastWrittenRow);
-                    if (columnIndex == 1) {
-                        columnIndex = (columnIndex + (fields[i].length-2)) - 1; // fields[i].length-2 because the first 2 positions are not title. Decrements 1 in the first iteration so it starts the count with value 0
-                    } else {
-                        columnIndex = (columnIndex + (fields[i].length-2)); // fields[i].length-2 because the first 2 positions are not title
-                    }
-                } else {
-                    columnIndex++;
-                }
-            }
-            // Returns the last written row
-            int maxValue = Collections.max(lastRowsWritten);  
-            return maxValue;
-        } catch (Exception e) {
-            System.out.println("Exception: " + e);
-            return 0;
-        }
-    }
 }
