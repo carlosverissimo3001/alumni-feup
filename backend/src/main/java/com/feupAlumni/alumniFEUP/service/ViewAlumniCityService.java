23ba11ddbec0f37583da7af6687d01e4c298ff37
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-03-05 11:09:11 +0000

    cities showing

---
+++ backend/src/main/java/com/feupAlumni/alumniFEUP/service/ViewAlumniCityService.java
@@ -0,0 +1,7 @@
+package com.feupAlumni.alumniFEUP.service;
+
+public interface ViewAlumniCityService {
+    // Populates the table with updated information. If the table has register, they are delted and the table is repopulated.
+    // Generates the GeoJSON file.
+    public void setLocationCitySetup();   
+}
