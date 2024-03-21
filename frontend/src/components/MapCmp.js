3841fc30923e7623d9cfbd24ab60af20a32a6a83
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-03-21 16:08:05 +0000

    showing the names aside

--- frontend/src/components/MapCmp.js
+++ frontend/src/components/MapCmp.js
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { clusterLayer, clusterCountLayer, unclusterPointLayer } from './MapLayers';
 //import alumniPerCountry from '../countriesGeoJSON.json';
-import alumniPerCountry from '../citiesGeoJSON.json';
+//import alumniPerCountry from '../citiesGeoJSON.json';
+import alumniPerCountry from '../edit_citiesGeoJSON.json';
 import {Map, Source, Layer} from 'react-map-gl';
 
 const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
@@ -9,6 +10,9 @@ const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 const MapCmp = () => {
 
     const mapRef = useRef(null);
+    const [residentNames, setResidentNames] = useState(null);
+    const [hoveredCluster, setHoveredCluster] = useState(Boolean);
+    const [hoveredMouseCoords, setHoveredMouseCoords] = useState([]);
 
     const onClick = event => {
         if (event.features && event.features.length > 0) {
@@ -29,7 +33,48 @@ const MapCmp = () => {
             });
           });
         }
+    }
+
+    const onHover = event => {
+      if (event.lngLat) {
+        setHoveredMouseCoords([event.point.x, event.point.y]);
       }
+
+      if (event.features && event.features.length > 0) {
+        const feature = event.features[0];
+        var residentes = feature.properties.residents;
+
+        // Parse residentes if it's a string
+        if (typeof residentes === 'string') {
+          const regex = /"([^"]+)"|'([^']+)'/g;
+          residentes = residentes.match(regex).map(match => match.replace(/['"]/g, ''));
+        }
+
+        // Function to flatten nested arrays
+        const flattenArray = arr => {
+          if (!Array.isArray(arr)) return [arr];
+          let flattened = [];
+          arr.forEach(item => {
+            flattened = flattened.concat(flattenArray(item));
+          });
+          return flattened;
+        };
+        residentes = flattenArray(residentes);
+
+        if (residentes.length > 0) {
+          setResidentNames(residentes);
+          setHoveredCluster(true);
+        } else {
+          setResidentNames([]);
+          setHoveredCluster(false);
+          setHoveredMouseCoords(null);
+        }
+      } else {
+        setResidentNames([]);
+        setHoveredCluster(false);
+        setHoveredMouseCoords(null);
+      }
+    };
 
     return (
         <div className="mapCmpDiv">
@@ -48,6 +93,7 @@ const MapCmp = () => {
             mapboxAccessToken={TOKEN}
             interactiveLayerIds={[clusterLayer.id]}
             onClick={onClick}
+            onMouseMove={onHover}
             ref={mapRef}
             >
             <Source
@@ -58,7 +104,8 @@ const MapCmp = () => {
                 clusterMaxZoom={14}
                 clusterRadius={50}
                 clusterProperties={{
-                students: ['+', ['get', 'students']],
+                  students: ['+', ['get', 'students']],
+                  residents: ['concat', ['get', 'residents']],
                 }}
             >
                 <Layer {...clusterLayer}/>
@@ -66,6 +113,24 @@ const MapCmp = () => {
                 <Layer {...unclusterPointLayer}/>
             </Source>
           </Map>
+          
+          { hoveredCluster && residentNames.length > 0  && (
+            <div
+              className="clusterRectangle"
+              style={{
+              position: 'absolute',
+              top:`${hoveredMouseCoords[1]}px`,
+              left: `${hoveredMouseCoords[0]}px`
+              }}
+            >
+              <ul>
+                {residentNames.map((resident, index) => (
+                  <li key={index}>{resident}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+
         </div>
     );
 };
