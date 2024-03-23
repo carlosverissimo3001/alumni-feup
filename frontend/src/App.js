8f7f36ba1396c86a254e4a817f5fc7c7e390e96c
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-03-23 18:57:10 +0000

    Switch between seeing distribution across countries and cities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapCmp from './components/MapCmp';
 import MenuButtons from './components/MenuButtons';
 
 
 function App() {
+  const [geoJSONFile, setGeoJSONFile] = useState('countries'); // by default it shows the countries
+
+  const handleSelectGeoJSON = (file) => {
+    setGeoJSONFile(file);
+  };
+
   return (
     <>
-        <MapCmp/>
-        <MenuButtons/>
+        <MapCmp geoJSONFile={geoJSONFile}/>
+        <MenuButtons onSelectGeoJSON={handleSelectGeoJSON}/>
     </>
   );
 }
