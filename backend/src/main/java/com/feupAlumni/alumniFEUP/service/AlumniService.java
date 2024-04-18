f5b668ae8328a7e3849a706b7932240fd31466f2
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-04-18 16:01:32 +0100

    getting the profile pic of users (for now I only tested for 1), and storing it in the folder 'images'

--- backend/src/main/java/com/feupAlumni/alumniFEUP/service/AlumniService.java
+++ backend/src/main/java/com/feupAlumni/alumniFEUP/service/AlumniService.java
@@ -1,12 +1,16 @@
 package com.feupAlumni.alumniFEUP.service;
 
+import java.io.IOException;
+
 import org.springframework.web.multipart.MultipartFile;
 
 public interface AlumniService {
 
     // By calling the API stores the scraped information in the Alumni Table
     //                                                   in a Backup file
-    public void populateAlumniTable(MultipartFile file);
+    //                                                   uploades the profile pics to the folder: "C:/alimniProject/backend/src/main/java/com/feupAlumni/alumniFEUP/Images"
+    // The name of the profile pics is set to the public identifier of the user, which is retrieved by the API
+    public void populateAlumniTable(MultipartFile file) throws IOException, InterruptedException;;
 
     // Backs up alumnis from table "Alumni". If the table is already populated:  all registeries are deleted
     //                                                                           the table is repopulated.
