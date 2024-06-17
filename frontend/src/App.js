43d9e8d5372778dc72de368fddfa5786d4f5d806
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-06-17 13:45:33 +0100

    interface login admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MapCmp from './components/MapCmp';
-
+import AdminLogin from './components/AdminComponents/AdminLogin';
 
 function App() {
   return (    
     <Router>
       <Routes>
         <Route path="/:yearUrl?" element={<MapCmp/>} />
+        <Route path="/admin" element={<AdminLogin/>} />
         <Route path="/" element={<MapCmp/>} />
       </Routes>
     </Router>
