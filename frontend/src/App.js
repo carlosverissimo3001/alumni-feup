5174c06e41177680eedac5bb91dc7fcb9973ec21
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-06-29 16:38:01 +0100

    admin interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MapCmp from './components/MapCmp';
 import AdminLogin from './components/AdminComponents/AdminLogin';
+import AdminSettings from './components/AdminComponents/AdminSettings';
 //import WebSocketServiceCmp from './components/WebSocketServiceCmp';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/:yearUrl?" element={<MapCmp/>} />
           <Route path="/admin" element={<AdminLogin/>} />
+          <Route path="/AdminDefinitions" element={<AdminSettings/>} />
           <Route path="/" element={<MapCmp/>} />
         </Routes>
       </Router>
