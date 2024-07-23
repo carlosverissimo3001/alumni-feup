62e8172b32234a9c52bf5d765de2ddb7d4baffe3
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-07-23 12:39:41 +0100

    errors and warnings

--- frontend/src/components/AdminComponents/AdminPopulate.js
+++ frontend/src/components/AdminComponents/AdminPopulate.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import Warning from './Warning'; 
 import { FcPlus } from "react-icons/fc";
+import Success from './Success';
+import Error from './Error';
 
 const AdminPopulate = () => {
     const [showWarningAdminReplace, setShowWarningAdminReplace] = useState(false);
     const [showWarningAdminAdd, setShowWarningAdminAdd] = useState(false);
     const [showWarningAdminUpdate, setShowWarningAdminUpdate] = useState(false);
     const [uploadedFile, setUploadedFile] = useState(null);
+    const [showSuccess, setShowSuccess] = useState(false);
+    const [successMessage, setSuccessMessage] = useState(false);
+    const [ errorMessage, setErrorMessage] = useState(false);
+    const [showError, setShowError] = useState(false);
 
     const handleFileUpload = (event) => {
         const file = event.target.files[0];
@@ -15,23 +21,59 @@ const AdminPopulate = () => {
 
     // Deletes the Alumni information and repopulates all tables with new updated information from the LinkedinLink API
     const handleReplaceAlumnus = async () => {
-        // TODO: SHOULD ENSURE THE FILE IS AN EXCEL FILE AND THAT THERE IS A FILE
-        // await setUp.replaceAlumnus(uploadedFile); Commented meanwhile
-        setShowWarningAdminReplace(false);
+        if (uploadedFile == null) {
+            setShowWarningAdminReplace(false);
+            setShowError(true);
+            setErrorMessage("Ups! No Excel file has been selected.");
+        } else {
+            /*var successReplace = await setUp.replaceAlumnus(uploadedFile); Commented meanwhile
+            if (successReplace) {
+                setShowSuccess(true);
+                setSuccessMessage("Great! Alumni information has been replaced."); 
+            }  else {
+                setShowError(true);
+                setErrorMessage("Ups! Something went wrong while trying to replace alumni data.");
+            }
+            setShowWarningAdminReplace(false);*/
+        }
     }
 
     // Only calls the API to alumnis that don't already exist in the app. If the alumni exists it doesn't call the API again
     const handleAddAlumnusData = async () => {
-        // TODO: SHOULD ENSURE THE FILE IS AN EXCEL FILE AND THAT THERE IS A FILE
-        // await setUp.addAlumnusData(uploadedFile); Commented meanwhile
-        setShowWarningAdminAdd(false);
+        if (uploadedFile == null) {
+            setShowWarningAdminReplace(false);
+            setShowError(true);
+            setErrorMessage("Ups! No Excel file has been selected.");
+        } else {
+            /*var successReplace = await setUp.addAlumnusData(uploadedFile); Commented meanwhile
+            if (successReplace) {
+                setShowSuccess(true);
+                setSuccessMessage("Great! New Alumni information has been added the system."); 
+            } else {
+                setShowError(true);
+                setErrorMessage("Ups! Something went wrong while trying to add alumni data.");
+            }
+            setShowWarningAdminAdd(false);*/   
+        }
     }
 
     // Calls the API for the alumnis that don't exist. If the alumni exists, updates the alumni information
     const handleUpdateAlumnusData = async () => {
-        // TODO: SHOULD ENSURE THE FILE IS AN EXCEL FILE AND THAT THERE IS A FILE
-        // await setUp.updateAlumnusData(uploadedFile); Commented meanwhile
-        setShowWarningAdminUpdate(false);
+        if (uploadedFile == null) {
+            setShowWarningAdminReplace(false);
+            setShowError(true);
+            setErrorMessage("Ups! No Excel file has been selected.");
+        } else {
+            /* var successReplace = await setUp.updateAlumnusData(uploadedFile); Commented meanwhile
+            if (successReplace) {
+                setShowSuccess(true);
+                setSuccessMessage("Great! Alumni information has been updated."); 
+            } else {
+                setShowError(true);
+                setErrorMessage("Ups! Something went wrong while trying to update alumni data.");
+            }
+            setShowWarningAdminUpdate(false);*/
+        }
     }
 
     const handleReplaceAlumniClick = async () => {
@@ -55,6 +97,14 @@ const AdminPopulate = () => {
     const handleConfirmUpdate = () => {
         handleUpdateAlumnusData();
     };
+
+    const handleConfirmSuccess = () => {
+        setShowSuccess(false);
+    }
+
+    const handleConfirmError = () => {
+        setShowError(false);
+    }
 
     const handleCancel = () => {   
         setShowWarningAdminUpdate(false);
@@ -119,7 +169,21 @@ const AdminPopulate = () => {
                     onConfirm={handleConfirmUpdate}
                     onCancel={handleCancel}
                 />
-            )}   
+            )}  
+
+            {showSuccess && (
+                <Success
+                    message={successMessage}
+                    onConfirm={handleConfirmSuccess}
+                />
+            )} 
+
+            {showError && (
+                <Error
+                    message={errorMessage}
+                    onConfirm={handleConfirmError}
+                />
+            )}
         </>
     );
 }
