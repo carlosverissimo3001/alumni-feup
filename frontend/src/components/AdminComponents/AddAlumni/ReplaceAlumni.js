490aa28e9ceac794ec68afa3fa5ccd4234ad5e8b
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-07-23 11:36:16 +0100

    refactor

--- frontend/src/components/AdminComponents/AddAlumni/ReplaceAlumni.js
+++ frontend/src/components/AdminComponents/AddAlumni/ReplaceAlumni.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import Warning from './Warning'; 
 import { FcPlus } from "react-icons/fc";
+import Warning from '../Warning'; 
 
-const AdminPopulate = () => {
-    const [showWarningAdminReplace, setShowWarningAdminReplace] = useState(false);
-    const [showWarningAdminAdd, setShowWarningAdminAdd] = useState(false);
-    const [showWarningAdminUpdate, setShowWarningAdminUpdate] = useState(false);
+const ReplaceAlumni = () => {
     const [uploadedFile, setUploadedFile] = useState(null);
+    const [showWarningAddAlumni, setShowWarningAddAlumni] = useState(false);
 
     const handleFileUpload = (event) => {
         const file = event.target.files[0];
@@ -17,53 +15,33 @@ const AdminPopulate = () => {
     const handleReplaceAlumnus = async () => {
         // TODO: SHOULD ENSURE THE FILE IS AN EXCEL FILE AND THAT THERE IS A FILE
         // await setUp.replaceAlumnus(uploadedFile); Commented meanwhile
-        setShowWarningAdminReplace(false);
+        setShowWarningAddAlumni(false);
     }
 
     // Only calls the API to alumnis that don't already exist in the app. If the alumni exists it doesn't call the API again
     const handleAddAlumnusData = async () => {
         // TODO: SHOULD ENSURE THE FILE IS AN EXCEL FILE AND THAT THERE IS A FILE
         // await setUp.addAlumnusData(uploadedFile); Commented meanwhile
-        setShowWarningAdminAdd(false);
+        setShowWarningAddAlumni(false);
     }
 
     // Calls the API for the alumnis that don't exist. If the alumni exists, updates the alumni information
     const handleUpdateAlumnusData = async () => {
         // TODO: SHOULD ENSURE THE FILE IS AN EXCEL FILE AND THAT THERE IS A FILE
         // await setUp.updateAlumnusData(uploadedFile); Commented meanwhile
-        setShowWarningAdminUpdate(false);
+        setShowWarningAddAlumni(false);
     }
 
-    const handleReplaceAlumniClick = async () => {
-        setShowWarningAdminReplace(true);
-    }
-
-    const handleAddAlumniClick = async () => {
-        setShowWarningAdminAdd(true);
-    }
-
-    const handleUpdateAlumniClick = async () => {
-        setShowWarningAdminUpdate(true);
-    }
-
-    const handleConfirmReplace = () => {
-        handleReplaceAlumnus();
-    };
-    const handleConfirmAdd = () => {
-        handleAddAlumnusData();
-    };
-    const handleConfirmUpdate = () => {
-        handleUpdateAlumnusData();
+    const handleConfirm = () => {
+        handleUpdateApiKey(apiKey)
     };
 
-    const handleCancel = () => {   
-        setShowWarningAdminUpdate(false);
-        setShowWarningAdminAdd(false);
-        setShowWarningAdminReplace(false);
+    const handleCancel = () => {
+        setShowWarningAddAlumni(false);
     };
 
     return (
-        <>                  
+        <>
             <div className='grid-container'>
                 <div className='grid-item label-column'>
                     <label htmlFor='fileUpload' className='admin-description input-label'>Add Alumnus</label>
@@ -89,34 +67,16 @@ const AdminPopulate = () => {
 
                 <div className='grid-item button-column'>
                     <div className='file-buttons'>
-                        <button className='admin-button button-no-margin' onClick={handleReplaceAlumniClick}>Replace Alumnus Data</button>
-                        <button className='admin-button' onClick={handleAddAlumniClick}>Add Alumnus Data</button>
-                        <button className='admin-button' onClick={handleUpdateAlumniClick}>Update Alumnus Data</button>
+                        <button className='admin-button button-no-margin' onClick={handleReplaceAlumnus}>Replace Alumnus Data</button>
+                        <button className='admin-button' onClick={handleAddAlumnusData}>Add Alumnus Data</button>
+                        <button className='admin-button' onClick={handleUpdateAlumnusData}>Update Alumnus Data</button>
                     </div>
                 </div>
-
             </div>
-
-            {showWarningAdminReplace && (
+            {showWarningAddAlumni && (
                 <Warning
-                    message="Alumni Table is going to be deleted, it's adviced to backup alumni first. Proxycurl API is going to be called and spend credits."
-                    onConfirm={handleConfirmReplace}
-                    onCancel={handleCancel}
-                />
-            )}   
-
-            {showWarningAdminAdd && (
-                <Warning
-                    message="New Alumni are going to be added to the Alumni Table, it's adviced to backup alumni first. Proxycurl API is going to be called and spend credits."
-                    onConfirm={handleConfirmAdd}
-                    onCancel={handleCancel}
-                />
-            )}   
-
-            {showWarningAdminUpdate && (
-                <Warning
-                    message="Alumni in the Alumni Table will be updated and new ones added, it's adviced to backup alumni first. Proxycurl API is going to be called and spend credits."
-                    onConfirm={handleConfirmUpdate}
+                    message="It's advised to first Backup Alumni Data. ProxyCurl API is going to be called and credits will be spent."
+                    onConfirm={handleConfirm}
                     onCancel={handleCancel}
                 />
             )}   
@@ -124,4 +84,4 @@ const AdminPopulate = () => {
     );
 }
 
-export default AdminPopulate;
+export default ReplaceAlumni;
