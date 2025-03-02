9a4f7144b19b6c87c071ec66edbc463d9ceb9b09
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2025-03-02 22:14:23 +0000

    feat: update Course entity and service to use CourseExtended; improve API response types

--- api/src/course/course.controller.ts
+++ api/src/course/course.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Course } from 'src/entities/course.entity';
 import { CourseService } from './course.service';
+import { CourseExtended } from '@entities'
 
 @ApiTags('V1', 'Course')
 @Controller('course')
@@ -11,16 +11,16 @@ export class CourseController {
   @Get()
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Get all courses' })
-  @ApiResponse({ description: 'Returns all courses', type: [Course] })
-  async findAll(): Promise<Course[]> {
+  @ApiResponse({ description: 'Returns all courses', type: [CourseExtended] })
+  async findAll(): Promise<CourseExtended[]> {
     return this.courseService.findAll();
   }
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Get a course by id' })
-  @ApiResponse({ description: 'Returns a course', type: Course })
-  async findOne(@Param('id') id: string): Promise<Course> {
+  @ApiResponse({ description: 'Returns a course', type: CourseExtended })
+  async findOne(@Param('id') id: string): Promise<CourseExtended> {
     return this.courseService.findOne(id);
   }
 }
