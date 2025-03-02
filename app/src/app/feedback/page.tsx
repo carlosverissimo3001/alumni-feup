8e5db567f4f7ca94d98b1a88338adc39ea833fc7
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2025-03-02 22:55:23 +0000

    fix: navbar alignment issue + feedback icon

---
+++ app/src/app/feedback/page.tsx
@@ -0,0 +1,8 @@
+export default function Feedback() {
+  return (
+    <div>
+      <h1>Do you have any feedback for us?</h1>
+      <p>This page is still under construction :)</p>
+    </div>
+  );
+}
