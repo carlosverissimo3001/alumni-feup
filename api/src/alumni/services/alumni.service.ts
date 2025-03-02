235c8a7aacff2dfca2dc6bc469982dec8585a7cd
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2025-03-02 20:25:35 +0000

    feat: implement Alumni repository and service; add geoJSON endpoint for alumni data

--- api/src/alumni/services/alumni.service.ts
+++ api/src/alumni/services/alumni.service.ts
@@ -1,41 +1,207 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Alumni } from 'src/entities/alumni.entity';
+import { Injectable } from '@nestjs/common';
+import {
+  Alumni,
+  GeoJSONFeatureCollection,
+  Graduation,
+  GeoJSONProperties,
+} from 'src/entities/';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAlumniDto } from '../../dto/create-alumni.dto';
-import { alumniSelect } from 'src/prisma/includes/alumni.include';
+import { AlumniRepository } from '../repositories/alumni.repository';
+import { GetGeoJSONDto } from 'src/dto/getgeojson.dto';
+import { Feature, Point } from 'geojson';
 
-const CREATED_BY = 'alumni-api';
+type GraduationWithCourse = Graduation & {
+  Course: {
+    name: string;
+    acronym: string;
+  };
+};
+
+type AlumniGrouped = {
+  coordinates: [number, number];
+  alumni: Array<{
+    name: string;
+    linkedin_url: string;
+    graduations: Array<{
+      course_acronym: string;
+      conclusion_year: number | null;
+    }>;
+  }>;
+};
+
+type AlumniByCountry = {
+  [country: string]: AlumniGrouped;
+};
+
+type AlumniByCity = {
+  [city: string]: AlumniGrouped;
+};
 
 @Injectable()
 export class AlumniService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly alumniRepository: AlumniRepository,
+  ) {}
 
   async findAll(): Promise<Alumni[]> {
-    const alumni = await this.prisma.alumni.findMany({
-      select: alumniSelect,
-    });
-    return alumni;
+    return this.alumniRepository.findAll();
+  }
+
+  async findAllGeoJSON(
+    query: GetGeoJSONDto,
+  ): Promise<GeoJSONFeatureCollection> {
+    const alumni = await this.alumniRepository.findAllGeoJSON(query);
+
+    // Group alumni by country
+    const alumniByCountry = alumni.reduce<AlumniByCountry>((acc, alumnus) => {
+      if (
+        !alumnus.Location?.country ||
+        !alumnus.Location?.latitude ||
+        !alumnus.Location?.longitude
+      ) {
+        return acc;
+      }
+
+      const country = alumnus.Location.country;
+
+      if (!acc[country]) {
+        acc[country] = {
+          coordinates: [alumnus.Location.longitude, alumnus.Location.latitude],
+          alumni: [],
+        };
+      }
+
+      acc[country].alumni.push({
+        name: `${alumnus.first_name} ${alumnus.last_name}`,
+        linkedin_url: alumnus.linkedin_url || '',
+        graduations:
+          alumnus.Graduations?.map((grad: GraduationWithCourse) => ({
+            course_acronym: grad.Course.acronym,
+            conclusion_year: grad.conclusion_year || null,
+          })) || [],
+      });
+      return acc;
+    }, {});
+
+    // Convert grouped data to GeoJSON features
+    const features: Array<Feature<Point, GeoJSONProperties>> = Object.entries(
+      alumniByCountry,
+    ).map(([country, data]) => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: data.coordinates,
+      },
+      properties: {
+        name: [country],
+        students: data.alumni.length,
+        listLinkedinLinksByUser: data.alumni.reduce(
+          (acc, curr) => {
+            if (curr.linkedin_url) {
+              acc[curr.linkedin_url] = curr.name;
+            }
+            return acc;
+          },
+          {} as { [key: string]: string },
+        ),
+        coursesYearConclusionByUser: data.alumni.reduce(
+          (acc, curr) => {
+            if (curr.linkedin_url) {
+              acc[curr.linkedin_url] = curr.graduations.reduce(
+                (courses, grad) => {
+                  if (grad.conclusion_year) {
+                    const academicYear = grad.conclusion_year.toString();
+                    courses[grad.course_acronym] = academicYear;
+                  }
+                  return courses;
+                },
+                {} as { [key: string]: string },
+              );
+            }
+            return acc;
+          },
+          {} as { [key: string]: { [key: string]: string } },
+        ),
+      },
+    }));
+
+    return {
+      type: 'FeatureCollection',
+      features,
+    };
   }
 
   async findOne(id: string): Promise<Alumni> {
-    const alumni = await this.prisma.alumni.findUnique({
-      where: { id },
-      select: alumniSelect,
-    });
-    if (!alumni) {
-      throw new NotFoundException(`Alumni with ID ${id} not found`);
-    }
-    return alumni;
+    return this.alumniRepository.findOne(id);
   }
 
   async create(body: CreateAlumniDto): Promise<Alumni> {
-    const alumni = await this.prisma.alumni.create({
-      data: {
-        ...body,
-        created_by: CREATED_BY,
-      },
-      select: alumniSelect,
-    });
-    return alumni;
+    return this.alumniRepository.create(body);
+  }
+
+  groupAlumniByCountry(alumni: Alumni[]): AlumniByCountry {
+    return alumni.reduce<AlumniByCountry>((acc, alumnus) => {
+      if (
+        !alumnus.Location?.country ||
+        !alumnus.Location?.latitude ||
+        !alumnus.Location?.longitude
+      ) {
+        return acc;
+      }
+
+      const country = alumnus.Location.country;
+
+      if (!acc[country]) {
+        acc[country] = {
+          coordinates: [alumnus.Location.longitude, alumnus.Location.latitude],
+          alumni: [],
+        };
+      }
+
+      acc[country].alumni.push({
+        name: `${alumnus.first_name} ${alumnus.last_name}`,
+        linkedin_url: alumnus.linkedin_url || '',
+        graduations:
+          alumnus.Graduations?.map((grad: GraduationWithCourse) => ({
+            course_acronym: grad.Course.acronym,
+            conclusion_year: grad.conclusion_year || null,
+          })) || [],
+      });
+      return acc;
+    }, {});
+  }
+
+  groupAlumniByCity(alumni: Alumni[]): AlumniByCity {
+    return alumni.reduce<AlumniByCity>((acc, alumnus) => {
+      if (
+        !alumnus.Location?.city ||
+        !alumnus.Location?.latitude ||
+        !alumnus.Location?.longitude
+      ) {
+        return acc;
+      }
+
+      const city = alumnus.Location.city;
+
+      if (!acc[city]) {
+        acc[city] = {
+          coordinates: [alumnus.Location.longitude, alumnus.Location.latitude],
+          alumni: [],
+        };
+      }
+
+      acc[city].alumni.push({
+        name: `${alumnus.first_name} ${alumnus.last_name}`,
+        linkedin_url: alumnus.linkedin_url || '',
+        graduations:
+          alumnus.Graduations?.map((grad: GraduationWithCourse) => ({
+            course_acronym: grad.Course.acronym,
+            conclusion_year: grad.conclusion_year || null,
+          })) || [],
+      });
+      return acc;
+    }, {});
   }
 }
