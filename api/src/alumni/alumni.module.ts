87bb4312a7655585f8249b9d14c0a21833b8eec7
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2025-03-02 20:25:35 +0000

    feat: implement Alumni repository and service; add geoJSON endpoint for alumni data

--- api/src/alumni/alumni.module.ts
+++ api/src/alumni/alumni.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AlumniService } from './services/alumni.service';
 import { AlumniController } from './controllers/alumni.controller';
 import { PrismaService } from '../prisma/prisma.service';
-
+import { AlumniRepository } from './repositories/alumni.repository';
+import { Logger } from '@nestjs/common';
 @Module({
   controllers: [AlumniController],
-  providers: [AlumniService, PrismaService],
+  providers: [AlumniService, PrismaService, AlumniRepository, Logger],
 })
 export class AlumniModule {}
