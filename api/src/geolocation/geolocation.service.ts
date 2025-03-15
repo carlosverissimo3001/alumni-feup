1fcdcfc91485e35cbd0f2d2e7b6c1e100cc5ff0f
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2025-03-15 18:10:15 +0000

    feat(geolocation): refactor coordinate extraction logic and enhance error handling

--- api/src/geolocation/geolocation.service.ts
+++ api/src/geolocation/geolocation.service.ts
@@ -2,8 +2,8 @@ import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Location as GeoLocation } from 'src/entities/location.entity';
-import { GeoLocationApiResponse, JobReturn } from 'src/consts';
+import { GeoLocationApiResponse } from 'src/consts';
+import { getOperationStatus } from './utils';
 
 @Injectable()
 export class GeolocationService {
@@ -26,13 +26,19 @@ export class GeolocationService {
     }
   }
 
-  // Fetches locations from the database that don't have coordinates and adds them to the database
-  async extractCoordinates() {
+  /**
+   * Call a 3rd party API to extract coordinates for locations without coordinates in the database
+   * @returns The status of the operation:
+   * - success: if all locations were updated
+   * - error: if some locations were not updated
+   */
+  async findMissingCoordinates() {
     try {
       // Find locations without coordinates
       const locationsWithoutCoordinates = await this.prisma.location.findMany({
         where: {
           OR: [{ latitude: null }, { longitude: null }],
+          AND: [{ is_country_only: false }],
         },
       });
 
@@ -78,7 +84,7 @@ export class GeolocationService {
         }
       }
 
-      return this.getOperationStatus(
+      return getOperationStatus(
         successfulUpdates.length,
         failedLocations.length,
       );
@@ -90,30 +96,18 @@ export class GeolocationService {
     }
   }
 
-  private getOperationStatus(
-    successCount: number,
-    failureCount: number,
-  ): JobReturn {
-    if (failureCount > 0) {
-      return {
-        status: 'error',
-        message:
-          'Some or all locations failed to update - Please review them manually -> `problematic_location` table',
-      };
-    }
-    return {
-      status: 'success',
-      message: `Successfully updated all ${successCount} locations`,
-    };
-  }
-
+  /** Calls a 3rd party API to extract coordinates for a given city and country pair
+   * @param {string} city - The city to extract coordinates for
+   * @param {string} country - The country to extract coordinates for
+   * @returns The coordinates of the location
+   */
   private async getCoordinatesFromApi(
-    city: string,
-    country: string,
+    city?: string | null,
+    country?: string | null,
   ): Promise<{ lat: number; lon: number } | null> {
     try {
       const response = await axios.get(
-        `${this.baseURL}${encodeURIComponent(city)},${encodeURIComponent(country)}&limit=1&appid=${this.apiKey}`,
+        `${this.baseURL}${encodeURIComponent(city ?? '')},${encodeURIComponent(country ?? '')}&limit=1&appid=${this.apiKey}`,
       );
 
       if (
@@ -131,5 +125,28 @@ export class GeolocationService {
       );
       return null;
     }
+  }
+
+  /**
+   * Given a country, it returns the coordinates marking its center
+   * @param country - The country to get the coordinates for
+   * @returns The coordinates of the country center
+   */
+  async getCountryCoordinatesFromDatabase(
+    country: string,
+  ): Promise<{ lat: number; lon: number }> {
+    const coords = await this.prisma.location.findFirst({
+      where: {
+        country: country,
+        // This is a flag to indicate that the location is a country only (coords point to the center of the country)
+        is_country_only: true,
+      },
+    });
+
+    if (!coords?.latitude || !coords?.longitude) {
+      throw new Error(`No coordinates found for country: ${country}`);
+    }
+
+    return { lat: coords.latitude, lon: coords.longitude };
   }
 }
