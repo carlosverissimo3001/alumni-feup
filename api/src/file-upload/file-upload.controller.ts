236924f46bacd380cbe73d5ae02339b24176bbe3
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2025-03-23 18:09:41 +0000

    feat: implement file upload module with controller and service

---
+++ api/src/file-upload/file-upload.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  Body,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileUploadService } from './file-upload.service';
+import { ApiOperation, ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger';
+import { UploadExtractionDto } from '@/dto/upload-extraction.dto';
+
+@ApiTags('V1', 'Files')
+@Controller('files')
+export class FileUploadController {
+  constructor(private readonly fileUploadService: FileUploadService) {}
+  @UseInterceptors(FileInterceptor('file'))
+  @Post()
+  @HttpCode(HttpStatus.ACCEPTED)
+  @ApiOperation({ summary: 'Upload a file' })
+  @ApiBody({
+    description: 'Data to upload',
+    type: UploadExtractionDto,
+  })
+  @ApiResponse({
+    description: 'Returns the status of the upload',
+    type: String,
+  })
+  create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: UploadExtractionDto,
+  ) {
+    return this.fileUploadService.handleFileUpload(body, file);
+  }
+}
