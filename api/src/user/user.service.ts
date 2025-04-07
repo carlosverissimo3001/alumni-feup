11b11b9bbf116dca0749d6e52a9a24daca407132
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2025-04-07 18:56:09 +0100

    feat(user): integrate Logger into UserService and add manualSubmission method

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { LinkedinAuthDto } from './dtos/linkedin-auth.dto';
+import { LinkedinAuthDto, ManualSubmissionDto } from '@/dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly logger: Logger,
+  ) {}
 
   async linkedinAuth(body: LinkedinAuthDto) {
     // Here, we ill match the user who just logged in with the Alumni data we have in the database
+    this.logger.log(body);
+  }
+
+  async manualSubmission(body: ManualSubmissionDto) {
+    this.logger.log(body);
   }
 }
