44ecacb4f13ad7ae001e58d7c086ffef17131989
Carlos Verissimo <carlosverissimo3001@gmail.com>
2025-04-08 23:17:35 +0100

    chore: deleting data-infra - will start from a clean place

--- data-infra/models/alumni_raw.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String, DateTime
-from .. import Base
-
-class AlumniRaw(Base):
-    __tablename__ = "alumni_raw"
-
-    id = Column(String, primary_key=True)
-    created_at = Column(DateTime(timezone=True), server_default='now()') 
