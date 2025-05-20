ef94cdbf4721c645c29f71f694b6522147efb369
Carlos Verissimo <carlosverissimo3001@gmail.com>
2025-05-20 01:30:14 +0100

    refactor(images): remove unused arrow icon and add new placeholder images for no location, no logo, and no user

--- app/src/components/ui/image-with-fallback.tsx
+++ app/src/components/ui/image-with-fallback.tsx
@@ -8,7 +8,7 @@ type ImageWithFallbackProps = ImageProps & {
 // This component is a wrapper around the next/image component that allows for a fallback image to be displayed in case the original image fails to load.
 const ImageWithFallback = (props: ImageWithFallbackProps) => {
   const { fallbackSrc, src, alt, width = 32, height = 32, ...rest } = props;
-  const defaultFallback = fallbackSrc || "/images/no-image.png";
+  const defaultFallback = fallbackSrc || "/images/placeholders/no-logo.png";
   const [imgSrc, setImgSrc] = useState(src);
 
   if (!src || src === "") {
