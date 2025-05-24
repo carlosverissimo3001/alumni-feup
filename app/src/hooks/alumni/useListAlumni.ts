df3aedfc6277efec9b269206f3621f551fddb88d
Carlos Verissimo <carlosverissimo3001@gmail.com>
2025-05-24 16:03:11 +0100

    refactor(useListAlumni): improve error handling and code formatting

--- app/src/hooks/alumni/useListAlumni.ts
+++ app/src/hooks/alumni/useListAlumni.ts
@@ -1,25 +1,34 @@
 import { useMutation } from "@tanstack/react-query";
-import { useJsonFromResponse, ResponseError } from "@/commom";
+import { useJsonFromResponse } from "@/commom";
 import NestAPI from "@/api";
 
 type Input = {
-    onSuccess?: () => void | Promise<void>
+  onSuccess?: () => void | Promise<void>;
+};
+
+interface ErrorWithResponse extends Error {
+  response?: Response;
 }
 
 export const useListAlumni = ({ onSuccess }: Input) => {
-    const mutation = useMutation({
-        mutationFn: () => NestAPI.alumniControllerFindAll(),
-        onSuccess
-    });
+  const mutation = useMutation({
+    mutationFn: () => NestAPI.alumniControllerFindAll(),
+    onSuccess,
+  });
 
-    const errorFromResponse = useJsonFromResponse<{error?: string}>(mutation.error as ResponseError)?.error;
+  const errorFromResponse = useJsonFromResponse<{ error?: string }>(
+    (mutation.error as ErrorWithResponse)?.response as Response
+  );
 
-    const unknownError = mutation.error ? 'Unknown error' : undefined;
+  const unknownError = mutation.error ? "Unknown error" : undefined;
 
-    const finalError = errorFromResponse?.error || (mutation.error as Error)?.message || unknownError;
+  const finalError =
+    errorFromResponse?.error ||
+    (mutation.error as Error)?.message ||
+    unknownError;
 
-    return {
-        ...mutation,
-        error: finalError,
-    }
-}
+  return {
+    ...mutation,
+    error: finalError,
+  };
+};
