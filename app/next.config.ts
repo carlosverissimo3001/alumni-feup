92a00f4ffa2a90bf1b5adbf9e6f728a3553e5f39
Carlos Verissimo <carlosverissimo3001@gmail.com>
2025-05-24 21:26:05 +0100

    fix(package.json, next.config): update start scripts to set PORT to 3010 and conditionally apply basePath and assetPrefix for production

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'standalone',
   basePath: '/newalumnieiworld',
+  ...(isProd && {
+    assetPrefix: '/newalumnieiworld/',
+  }),
 };
 
 export default nextConfig;
