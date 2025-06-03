fea74231999eb692e8753e7ad978d5d4d569c3c3
Carlos Verissimo <carlosverissimo3001@gmail.com>
2025-06-03 01:30:06 +0100

    chore: location endpoint

--- agents-api/app/services/location.py
+++ agents-api/app/services/location.py
@@ -3,8 +3,20 @@ import logging
 from app.core.config import settings
 from app.db import get_db
 from app.db.models import Location
+from app.schemas.location import (
+    LocationType,
+    ResolveAlumniLocationParams,
+    ResolveRoleLocationParams,
+    ResolveCompanyLocationParams,
+    RoleLocationInput,
+    CompanyLocationInput,
+    AlumniLocationInput,
+)
 from app.utils.http_client import HTTPClient
 from app.utils.location_db import update_location
+from app.utils.company_db import get_companies_by_ids
+from app.utils.role_db import get_roles_by_ids
+from app.agents.location import location_agent
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +36,54 @@ class LocationService:
             base_url=settings.GEOLOCATION_BASE_URL,
         )
 
+    async def request_role_location(self, params: ResolveRoleLocationParams):
+        """
+        Request the agent to resolve the location of any role.
+        """
+        role_ids = params.role_ids
+        roles = get_roles_by_ids(role_ids.split(","), db)
+
+        for role in roles:
+            input = RoleLocationInput(
+                type=LocationType.ROLE,
+                role_id=role.id,
+                location=role.location,
+            )
+            
+            await location_agent.process_location(input)
+        
+    async def request_alumni_location(self, params: ResolveAlumniLocationParams):
+        """
+        Request the agent to resolve the location of any alumni.
+        """
+        role_ids = params.role_ids
+        roles = get_roles_by_ids(role_ids.split(","), db)
+
+        for role in roles:
+            input = RoleLocationInput(
+                type=LocationType.ROLE,
+                role_id=role.id,
+                location=role.location,
+            )
+            
+            await location_agent.process_location(input)
+            
+    async def request_company_location(self, params: ResolveCompanyLocationParams):
+        """
+        Request the agent to resolve the location of any company.
+        """
+        company_ids = params.company_ids
+        companies = get_companies_by_ids(company_ids.split(","), db)
+
+        for company in companies:
+            input = CompanyLocationInput(
+                type=LocationType.COMPANY,
+                company_id=company.id,
+                headquarters=company.headquarters,
+            )
+            
+            await location_agent.process_location(input)
+    
     async def update_location_coordinates(self, location: Location):
         """
         Get the coordinates of a city from the Geocoding API and update the location object
