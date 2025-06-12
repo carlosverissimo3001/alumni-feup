ff45d8882f8f6a473bbcea02d7420019fb1b7ec1
Carlos Veríssimo <94475392+carlosverissimo3001@users.noreply.github.com>
2025-06-12 20:48:14 +0100

    chore: remove invite flow, make platform public :) (#50)

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,31 +1,18 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import {
   authMiddleware,
   adminMiddleware,
-  inviteOnlyMiddleware,
 } from "./middlewares";
-import getHypertune from "./lib/getHypertune";
 
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/admin")) {
     return adminMiddleware(request);
   }
-  
-  const hypertune = await getHypertune();
-  // No need to check for this in dev
-  const isInviteFlowEnabled =
-    process.env.NODE_ENV === "production" &&
-    hypertune.isInviteFlowEnabled({ fallback: false });
-
-  if (isInviteFlowEnabled) {
-    return inviteOnlyMiddleware(request);
-  }
-
   if (request.nextUrl.pathname.startsWith("/reviews")) {
     return authMiddleware(request);
   }
 
-  return;
+  return NextResponse.next();
 }
 
 export const config = {
